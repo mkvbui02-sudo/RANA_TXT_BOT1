8f4e21180d094c32cbf970f1eef263a1ff1e08d0
mkvbui02-sudo <mkvbui02@gmail.com>
2025-09-14 03:04:15 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -3,10 +3,10 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(environ.get("OWNER", "5302639580"))
+API_ID = int(environ.get("API_ID", "23557588"))
+API_HASH = environ.get("API_HASH", "7ed52f38f74fe85ebc2e5e285051c3b6")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8282609988:AAEUXHOxBzGgRS28JjdT7KOV66HvL79OqUI")
+OWNER = int(environ.get("OWNER", "2084001270"))
 CREDIT = "𓆰𝐑𝐀𝐍𝐀 𝐉𝐈𝐈 𓆪"
 AUTH_USER = os.environ.get('AUTH_USERS', '5302639580,6187058869').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
