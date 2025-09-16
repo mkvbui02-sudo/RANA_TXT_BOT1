40c08b53b86dc6bbeb4d99c25c5b9308dc87b54d
mkvbui02-sudo <mkvbui02@gmail.com>
2025-09-16 20:56:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -601,13 +601,13 @@ async def txt_handler(bot: Client, m: Message):
 
             if "acecwply" in url:
                 cmd = f'yt-dlp -o "{name}.%(ext)s" -f "bestvideo[height<={raw_text2}]+bestaudio" --hls-prefer-ffmpeg --no-keep-video --remux-video mkv --no-warning "{url}"'
-
-            elif "https://cpvod.testbook.com/" in url:
-                                url = url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/") 
-                url=("https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}")
-                mpd, keys = helper.get_mps_and_keys(url)
+ 
+elif "https://cpvod.testbook.com/" in url:
+url=url.replace("https://cpvod.testbook.com/","https://media-cdn.classplusapp.com/drm/
+url=("https://covercel.vercel.app/extract_keys?url={url}@bots_updatee&user_id={user_id}")
+mpd, keys = helper.get_mps_and_keys(url)
                 url = mpd
-                keys_string = " ".join([f"--key {key}" for key in keys])
+keys_string = " ".join([f"--key {key}" for key in keys])
 
             elif "classplusapp.com/drm/" in url:
                 url = 'https://dragoapi.vercel.app/classplus?link=' + url
